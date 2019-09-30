494d96c9095650726ae22302ca70b59fd5ea3f1e
Andrea Manzi <andrea.manzi@comune.fi.it>
2019-09-30 21:24:24 +0200

    Progetto iniziale

--- app/src/main/java/it/manzolo/job/service/bluewatcher/App.kt
+++ app/src/main/java/it/manzolo/job/service/bluewatcher/App.kt
@@ -6,7 +6,9 @@ import android.app.job.JobScheduler
 import android.content.ComponentName
 import android.content.Context
 import android.preference.PreferenceManager
+import android.widget.Toast
 import java.util.concurrent.TimeUnit
+
 
 class App : Application() {
 
@@ -20,17 +22,27 @@ class App : Application() {
 
             val preferences = PreferenceManager.getDefaultSharedPreferences(context)
             val seconds = preferences.getString("seconds", "15") //"" is the default String to return if the preference isn't found
+            val enabled = preferences.getBoolean("enabled", true) //"" is the default String to return if the preference isn't found
+            val address = preferences.getString("devices", "")
+            if (address.replace("\\s".toRegex(), "").length === 0) {
+                Toast.makeText(context, "No devices in preferences", Toast.LENGTH_LONG).show()
+            } else {
+                //Log.i("Manzolo",seconds);
+                if (enabled) {
+                    Toast.makeText(context, "Start service every " + seconds + " seconds", Toast.LENGTH_SHORT).show()
+                    val jobInfo = JobInfo.Builder(1, componentName)
+                            .setMinimumLatency(TimeUnit.SECONDS.toMillis(60))
+                            .setOverrideDeadline(TimeUnit.SECONDS.toMillis(seconds.toLong() * 1000))
+                            .setRequiredNetworkType(JobInfo.NETWORK_TYPE_ANY)
+                            .setPersisted(true)
+                            .build()
 
-            //Log.i("Manzolo",seconds);
+                    jobScheduler.schedule(jobInfo)
+                } else {
+                    Toast.makeText(context, "Service disabled in preferences", Toast.LENGTH_LONG).show()
+                }
+            }
 
-            val jobInfo = JobInfo.Builder(1, componentName)
-                    .setMinimumLatency(TimeUnit.SECONDS.toMillis(60))
-                    .setOverrideDeadline(TimeUnit.SECONDS.toMillis(seconds.toLong()))
-                    .setRequiredNetworkType(JobInfo.NETWORK_TYPE_ANY)
-                    .setPersisted(true)
-                    .build()
-
-            jobScheduler.schedule(jobInfo)
         }
     }
 }
