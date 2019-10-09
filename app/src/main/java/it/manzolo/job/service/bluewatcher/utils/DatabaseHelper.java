01b5519b801d6b0d2f79ff5479bdcdc880558b67
Andrea Manzi <manzolo@libero.it>
2019-10-09 22:09:27 +0200

    add backup restore db

--- app/src/main/java/it/manzolo/job/service/bluewatcher/utils/DatabaseHelper.java
+++ app/src/main/java/it/manzolo/job/service/bluewatcher/utils/DatabaseHelper.java
@@ -3,18 +3,26 @@ package it.manzolo.job.service.bluewatcher.utils;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.os.Environment;
+import android.widget.Toast;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.nio.channels.FileChannel;
 
 public class DatabaseHelper extends SQLiteOpenHelper {
 
     private static final String DATABASE_NAME = "voltwatcher.db";
     private static final int DATABASE_VERSION = 1;
-
+    private Context context;
     // Lo statement SQL di creazione del database
     private static final String DB_CREATE = "create table voltwatcher (_id integer primary key autoincrement, device text not null, data text not null, volts text not null, temps text not null, longitude text not null, latitude text not null, detectorbattery integer ,sent integer);";
 
     // Costruttore
     public DatabaseHelper(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
+        this.context = context;
     }
 
     // Questo metodo viene chiamato durante la creazione del database
@@ -29,5 +37,55 @@ public class DatabaseHelper extends SQLiteOpenHelper {
 
         database.execSQL("DROP TABLE IF EXISTS voltwatcher");
         onCreate(database);
+    }
+
+    public void backup() {
+        try {
+            File sd = Environment.getExternalStorageDirectory();
+            File data = Environment.getDataDirectory();
+
+            if (sd.canWrite()) {
+                String currentDBPath = "//data/" + context.getPackageName() + "/databases/" + DATABASE_NAME;
+                String backupDBPath = DATABASE_NAME;
+                File currentDB = new File(data, currentDBPath);
+                File backupDB = new File(sd, backupDBPath);
+
+                if (currentDB.exists()) {
+                    FileChannel src = new FileInputStream(currentDB).getChannel();
+                    FileChannel dst = new FileOutputStream(backupDB).getChannel();
+                    dst.transferFrom(src, 0, src.size());
+                    src.close();
+                    dst.close();
+                    Toast.makeText(this.context, "Backup is successful to SD card", Toast.LENGTH_SHORT).show();
+                }
+            }
+        } catch (Exception e) {
+            Toast.makeText(this.context, "Backup error: " + e.getMessage(), Toast.LENGTH_LONG).show();
+        }
+    }
+
+    public void restore() {
+        try {
+            File sd = Environment.getExternalStorageDirectory();
+            File data = Environment.getDataDirectory();
+
+            if (sd.canWrite()) {
+                String currentDBPath = "//data/" + context.getPackageName() + "/databases/" + DATABASE_NAME;
+                String backupDBPath = DATABASE_NAME;
+                File currentDB = new File(data, currentDBPath);
+                File backupDB = new File(sd, backupDBPath);
+
+                if (currentDB.exists()) {
+                    FileChannel src = new FileInputStream(backupDB).getChannel();
+                    FileChannel dst = new FileOutputStream(currentDB).getChannel();
+                    dst.transferFrom(src, 0, src.size());
+                    src.close();
+                    dst.close();
+                    Toast.makeText(this.context, "Database Restored successfully", Toast.LENGTH_SHORT).show();
+                }
+            }
+        } catch (Exception e) {
+            Toast.makeText(this.context, "Restore error: " + e.getMessage(), Toast.LENGTH_LONG).show();
+        }
     }
 }
